654d3c26a99fb3299d42f0c9910d03cca88f8d40
Dio Riandika <dio.rndk@gmail.com>
2021-09-05 21:27:15 +0700

    - add post rate stars

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
@@ -6,8 +6,31 @@ function MovieDetails(props) {
 
     const mov = props.movie
 
+    const [ highlighted, setHighlited ] = useState(-1)
+
+    const highlightRate = high => evt => {
+        setHighlited(high)
+    }
+
+    const rateClicked = rate => evt => {
+        fetch(`http://127.0.0.1:8000/api/movies/${mov.id}/rate_movie/`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Token 56c7b44f49cd42aa14d9e21a50072deb098b55f2'
+            },
+            body: JSON.stringify({
+                stars: rate + 1
+            })
+        })
+        .then(resp => resp.json())
+        .then(resp => console.log(resp)) 
+        .catch(error => console.log(error))
+    }
+    
+
     return (
-        <div>
+        <React.Fragment>
             {
                 mov ? (
                     <div>
@@ -19,10 +42,25 @@ function MovieDetails(props) {
                         <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange' : ''}/>
                         <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange' : ''}/>
                         {mov.no_of_ratings}
+                        <div className="rate-container">
+                            <h2>Rate it</h2>
+                            {
+                                [...Array(5)].map( (e, i) => {
+                                    return <FontAwesomeIcon 
+                                    key={i} 
+                                    icon={faStar} 
+                                    className={highlighted > i - 1 ? 'purple' : ''} 
+                                    onMouseEnter={highlightRate(i)} 
+                                    onMouseLeave={highlightRate(-1)}
+                                    onClick={rateClicked(i)}
+                                    />
+                                })
+                            }
+                        </div>
                     </div>
                 ) : null
             }
-        </div>
+        </React.Fragment>
     )
 }
 
