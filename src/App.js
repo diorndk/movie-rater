723fae789d2fdab095df1a2fb877453e9a543ec8
Dio Riandika <dio.rndk@gmail.com>
2021-09-05 17:47:27 +0700

    - iterate dynamic movies data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': 'Token 56c7b44f49cd42aa14d9e21a50072deb098b55f2'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setMovie(resp))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
+      </header>
         <div className="layout">
-          <div>Movie List</div>
+          <div>
+            { movies.map( movie => {
+              return <h2>{movie.title}</h2>
+            })}
+          </div>
           <div>Movie Details</div>
         </div>
-      </header>
     </div>
   );
 }
